3901430746fe65a6c2ca0299b51a1e2ed531c579
Yoshihiro Tanaka <contact@cordea.jp>
2022-12-25 14:59:02 +0900

    optimize test execution time

--- lib/parallel_test_gen.dart
+++ lib/parallel_test_gen.dart
@@ -1,7 +1,43 @@
+import 'dart:collection';
 import 'dart:io';
+
+import 'package:collection/collection.dart';
 
 int calculate() {
   return 6 * 7;
+}
+
+List<List<TestFileStat>> optimize(
+  List<TestFileStat> stats, {
+  required int concurrent,
+}) {
+  if (concurrent > stats.length) {
+    throw Exception();
+  }
+  final sorted = Queue<TestFileStat>.from(
+    stats.sorted((a, b) => b.duration.compareTo(a.duration)),
+  );
+  final group = <List<TestFileStat>>[];
+  final durations = <Duration>[];
+  for (int i = 0; i < concurrent; i++) {
+    final f = sorted.removeFirst();
+    group.add([f]);
+    durations.add(f.duration);
+  }
+  while (sorted.isNotEmpty) {
+    var index = 0;
+    final f = sorted.removeFirst();
+    var min = Duration(days: 1);
+    for (int i = 0; i < concurrent; i++) {
+      if (min > durations[i]) {
+        min = durations[i];
+        index = i;
+      }
+    }
+    group[index].add(f);
+    durations[index] += f.duration;
+  }
+  return group;
 }
 
 Future<List<TestFileStat>> listTestStats({required String path}) async {
--- test/parallel_test_gen_test.dart
+++ test/parallel_test_gen_test.dart
@@ -1,4 +1,3 @@
-
 import 'package:parallel_test_gen/parallel_test_gen.dart';
 import 'package:path/path.dart';
 import 'package:test/test.dart';
@@ -15,5 +14,63 @@ void main() {
     expect(result[1].duration, isNot(Duration.zero));
     expect(result[2].path, join('test', 'fixtures', '1', '3_test.dart'));
     expect(result[2].duration, isNot(Duration.zero));
+  });
+
+  group('optimize', () {
+    final stats = [
+      TestFileStat('1', Duration(seconds: 1)),
+      TestFileStat('2', Duration(seconds: 4)),
+      TestFileStat('3', Duration(seconds: 5)),
+      TestFileStat('4', Duration(seconds: 6)),
+      TestFileStat('5', Duration(seconds: 8)),
+      TestFileStat('6', Duration(seconds: 5)),
+      TestFileStat('7', Duration(seconds: 10)),
+      TestFileStat('8', Duration(seconds: 2)),
+      TestFileStat('9', Duration(seconds: 6)),
+      TestFileStat('10', Duration(seconds: 7)),
+    ];
+
+    test('with concurrent is 1', () {
+      final result = optimize(stats, concurrent: 1);
+
+      expect(result, hasLength(1));
+      expect(result[0], hasLength(10));
+    });
+
+    test('with concurrent is 3', () {
+      final result = optimize(stats, concurrent: 3);
+
+      expect(result, hasLength(3));
+
+      expect(result[0][0].path, '7'); // 10
+      expect(result[0][1].path, '3'); // 15
+      expect(result[0][2].path, '8'); // 17
+      expect(result[0][3].path, '1'); // 18
+
+      expect(result[1][0].path, '5'); // 8
+      expect(result[1][1].path, '9'); // 14
+      expect(result[1][2].path, '2'); // 18
+
+      expect(result[2][0].path, '10'); // 7
+      expect(result[2][1].path, '4'); // 13
+      expect(result[2][2].path, '6'); // 18
+    });
+
+    test('with concurrent is 10', () {
+      final result = optimize(stats, concurrent: 10);
+
+      expect(result, hasLength(10));
+    });
+
+    test('with concurrent is 11', () {
+      var hasError = false;
+      try {
+        optimize(stats, concurrent: 11);
+      } catch (_) {
+        hasError = true;
+      }
+
+      expect(hasError, true);
+    });
   });
 }
