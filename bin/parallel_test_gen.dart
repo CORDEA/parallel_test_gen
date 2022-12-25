77092ef4eef13368ab403bbc81af903a725ef1d5
Yoshihiro Tanaka <contact@cordea.jp>
2022-12-25 12:06:31 +0900

    initial commit :)

---
+++ bin/parallel_test_gen.dart
@@ -0,0 +1,5 @@
+import 'package:parallel_test_gen/parallel_test_gen.dart' as parallel_test_gen;
+
+void main(List<String> arguments) {
+  print('Hello world: ${parallel_test_gen.calculate()}!');
+}
---
+++ test/parallel_test_gen_test.dart
@@ -0,0 +1,8 @@
+import 'package:parallel_test_gen/parallel_test_gen.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
