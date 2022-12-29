ba975b5ce625676cc7d63e5dca7605d1da4a4e70
Yoshihiro Tanaka <contact@cordea.jp>
2022-12-30 08:25:28 +0900

    output the result

--- bin/parallel_test_gen.dart
+++ bin/parallel_test_gen.dart
@@ -1,14 +1,18 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:args/args.dart';
 import 'package:parallel_test_gen/parallel_test_gen.dart';
 import 'package:parallel_test_gen/src/runner.dart';
+import 'package:path/path.dart';
+
+const _configFile = 'test_config.json';
 
 void main(List<String> arguments) {
   final parser = ArgParser();
 
   final check = parser.addCommand('check');
-  check.addOption('concurrency');
+  check.addOption('concurrency', defaultsTo: '1');
 
   final test = parser.addCommand('test');
   test.addOption('config');
@@ -32,8 +36,15 @@ void main(List<String> arguments) {
 
 Future<void> _check(ArgResults results) async {
   final runner = TestRunner();
-  final stats = await listTestStats(
-    runner,
-    results.rest.isEmpty ? Directory.current : Directory(results.rest.first),
+  final directory =
+      results.rest.isEmpty ? Directory.current : Directory(results.rest.first);
+  final stats = await listTestStats(runner, directory);
+  final result = optimize(
+    directory: directory,
+    stats: stats,
+    concurrency: int.parse(results['concurrency']),
   );
+  final json = jsonEncode(result);
+  final file = File(join(directory.path, _configFile));
+  await file.writeAsString(json);
 }
