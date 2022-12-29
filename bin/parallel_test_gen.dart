0f926d5ec614c9ed22c39b853bfded06ad9c9ed6
Yoshihiro Tanaka <contact@cordea.jp>
2022-12-29 18:39:21 +0900

    parse command-line arguments

--- bin/parallel_test_gen.dart
+++ bin/parallel_test_gen.dart
@@ -1,2 +1,14 @@
+import 'package:args/args.dart';
 
-void main(List<String> arguments) {}
+void main(List<String> arguments) {
+  final parser = ArgParser();
+
+  final check = parser.addCommand('check');
+  check.addOption('concurrency');
+
+  final test = parser.addCommand('test');
+  test.addOption('config');
+  test.addOption('id');
+
+  final result = parser.parse(arguments);
+}
